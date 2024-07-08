654390dc0a9867eee65da7b0c05482ab7c94bcce
DionardoNS <45784918+dionardo@users.noreply.github.com>
2024-07-09 01:57:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+      setTimeout(function() {
+  document.getElementById('myIframe').classList.add('loaded');
+  document.querySelector('.loader-container').style.display = 'none';
+}, 2000);
+};
